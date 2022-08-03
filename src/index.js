8380ce05c121c1c80ca3d1bbd40d75c55e50826a
aron-helu <aronmilahela@gmail.com>
2022-08-03 19:16:13 +0300

    fix linters

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import logo from './assets/logo.png';
 import './style.css';
 
-const image = document.getElementById("imageLogo");
+const image = document.getElementById('imageLogo');
 const imageLogo = document.createElement('img');
 imageLogo.src = logo;
-imageLogo.className = "logoStyle";
+imageLogo.className = 'logoStyle';
 image.appendChild(imageLogo);
 
-$(".theme").click(() => {
-    let currTheme = $("body").attr("data-theme");
-    $("body").attr("data-theme", `${currTheme == "light" ? `dark` : `light`}`);
-}); 
+document.querySelector('.theme').add.click(() => {
+  const currTheme = document.querySelector('body').attr('data-theme');
+  document.querySelector('body').attr('data-theme', `${currTheme === 'light' ? 'dark' : 'light'}`);
+});
